9bf665ae0a5422d80cfcd92a3b13ebb47b8bf8b5
luisgmunhoz <luisguilherme.sm@hotmail.com>
2025-01-04 13:03:10 -0300

    feat: add cors

--- main.py
+++ main.py
@@ -4,12 +4,27 @@ from configs.environment import get_environment_variables
 from configs.middleware import get_current_user
 from metadata.tags import Tags
 from models.base_model import init
-
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 # Application Environment Configuration
 env = get_environment_variables()
 
 # Core Application Instance
 app = FastAPI(title=env.APP_NAME, version=env.API_VERSION, openapi_tags=Tags)
+
+
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/ping")
